f7e59db9a665d11ef3a79c818163b448fac4b5ad
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-31 17:25:18 +0200

    First tent "naive"
    The failures parts is tricky and redundant between them.

--- src/StringCalculator.cs
+++ src/StringCalculator.cs
@@ -1,1 +1,125 @@
-﻿namespace StringCalculatorKata;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using LanguageExt;
+using static System.String;
+
+namespace StringCalculatorKata;
+
+public static partial class StringCalculator
+{
+    private const string NewLine = "\n";
+    private const string DefaultDelimiter = ",";
+    private const int BigNumber = 1000;
+
+    public static Either<string, int> Add(string inputs)
+    {
+        if (IsEmpty(inputs))
+        {
+            return 0;
+        }
+
+        var (delimiters, numbersPart) = Delimiters(inputs);
+
+        var numbers = Numbers(numbersPart)
+            .Where(IsNotBigNumber)
+            .ToList();
+
+        var failures = Failures()
+            .Concat(NegativeNumberFailures(numbers))
+            .Concat(NumberRepresentationFailures(numbersPart, delimiters))
+            .Concat(BadDelimiterFailures(numbersPart, delimiters))
+            .ToList();
+
+        return failures.Any()
+                   ? Join(NewLine, failures)
+                   : numbers.Sum();
+    }
+
+    private static bool IsEmpty(string inputs)
+        => inputs == Empty;
+
+    private static IEnumerable<string> Failures()
+        => Enumerable.Empty<string>();
+
+    private static (string[], string) Delimiters(string inputs)
+    {
+        var match = DelimiterDefinition().Match(inputs);
+        if (!match.Success)
+        {
+            return ([DefaultDelimiter, NewLine], inputs);
+        }
+
+        var delimiter = match.Groups["delimiter"].Value;
+        var numbers = match.Groups["numbers"].Value;
+
+        return ([delimiter, NewLine], numbers);
+    }
+
+    private static int[] Numbers(string numbersPart)
+        => NumbersDefinition()
+            .Split(numbersPart)
+            .Where(s => !IsNullOrWhiteSpace(s))
+            .Select(int.Parse)
+            .ToArray();
+
+    private static bool IsNotBigNumber(int number)
+        => number < BigNumber;
+
+    private static IEnumerable<string> NegativeNumberFailures(List<int> numbers)
+    {
+        var negativeNumbers = numbers.Where(n => n < 0).ToList();
+        if (negativeNumbers.Any())
+        {
+            yield return $"Negative number(s) not allowed: {Join(", ", negativeNumbers)}";
+        }
+    }
+
+    private static IEnumerable<string> BadDelimiterFailures(
+        string inputs,
+        string[] delimiters)
+    {
+        var hasBadDelimiter = NotANumbersOrADelimiterDefinition(delimiters).Match(inputs);
+        if (!hasBadDelimiter.Success)
+        {
+            yield break;
+        }
+
+        var badDelimiter = hasBadDelimiter.Value;
+        var position = hasBadDelimiter.Index;
+
+        yield return $"'{delimiters.First()}' expected but '{badDelimiter}' found at position {position}";
+    }
+
+    private static IEnumerable<string> NumberRepresentationFailures(string inputs, string[] delimiters)
+    {
+        var numberRepresentations = inputs.Split(delimiters, StringSplitOptions.None);
+        if (numberRepresentations.Any(IsNullOrWhiteSpace))
+        {
+            var inputsWithoutDelimiters = delimiters.Aggregate(
+                inputs,
+                (current, delimiter) => current.Replace(delimiter, DefaultDelimiter));
+
+            var position = inputsWithoutDelimiters.IndexOf(
+                DefaultDelimiter + DefaultDelimiter,
+                StringComparison.Ordinal);
+
+            if (position == -1)
+            {
+                position = inputsWithoutDelimiters.Length;
+            }
+
+            yield return $"Number expected at position {position + 1}";
+        }
+    }
+
+    [GeneratedRegex(@"^//(?<delimiter>.*)\n(?<numbers>.*)", RegexOptions.ExplicitCapture)]
+    private static partial Regex DelimiterDefinition();
+
+    private static Regex NotANumbersOrADelimiterDefinition(string[] delimiters)
+        => new(@$"[^-\d{Join("", delimiters)}]", RegexOptions.Compiled);
+
+    [GeneratedRegex(@"[^-\d]")]
+    private static partial Regex NumbersDefinition();
+}
--- test/StringCalculatorShould.cs
+++ test/StringCalculatorShould.cs
@@ -1,13 +1,64 @@
-using FluentAssertions;
-using Xunit;
-
-namespace StringCalculatorKata.Tests;
-
-public class StringCalculatorShould
-{
-    [Fact(Skip = "Not implemented yet")]
-    public void Test1()
-    {
-        false.Should().BeTrue();
-    }
+using FluentAssertions.LanguageExt;  
+using Xunit;  
+  
+namespace StringCalculatorKata.Tests;  
+  
+public class StringCalculatorShould  
+{  
+    [Theory]  
+    [InlineData("", 0)]  
+    [InlineData("1", 1)]  
+    [InlineData("1,2", 3)]  
+    public void Step1(string inputs, int result)  
+        => StringCalculator.Add(inputs).Should().Be(result);  
+  
+    [Theory]  
+    [InlineData("1,2,10", 13)]  
+    [InlineData("1,2,10,11", 24)]  
+    public void Step2(string inputs, int result)  
+        => StringCalculator.Add(inputs).Should().Be(result);  
+  
+    [Theory]  
+    [InlineData("1,2\n3", 6)]  
+    [InlineData("1,2\n3\n4", 10)]  
+    public void Step3(string inputs, int result)  
+        => StringCalculator.Add(inputs).Should().Be(result);  
+  
+    [Theory]  
+    [InlineData("1,2,", "Number expected at position 5")]  
+    [InlineData("2,\n3", "Number expected at position 2")]  
+    public void Step4(string inputs, string reason)  
+        => StringCalculator.Add(inputs)  
+            .Should()  
+            .Be(reason);  
+  
+    [Theory]  
+    [InlineData("//;\n1;3", 4)]  
+    [InlineData("//|\n1|2|3", 6)]  
+    [InlineData("//sep\n2sep5", 7)]  
+    public void Step5(string inputs, int result)  
+        => StringCalculator.Add(inputs).Should().Be(result);  
+  
+    [Theory]  
+    [InlineData("//|\n1|2,3", "'|' expected but ',' found at position 3")]  
+    public void Step5_fail(string inputs, string reason)  
+        => StringCalculator.Add(inputs)  
+            .Should()  
+            .Be(reason);  
+  
+    [Theory]  
+    [InlineData("1,-2", "Negative number(s) not allowed: -2")]  
+    [InlineData("2,-4,-9", "Negative number(s) not allowed: -4, -9")]  
+    public void Step6(string inputs, string reason)  
+        => StringCalculator.Add(inputs).Should().Be(reason);  
+  
+    [Theory]  
+    [InlineData("//|\n1|2,-3", "Negative number(s) not allowed: -3\n'|' expected but ',' found at position 3")]  
+    public void Step7(string inputs, string reason)  
+        => StringCalculator.Add(inputs).Should().Be(reason);  
+  
+    [Theory]  
+    [InlineData("2,1000", 2)]  
+    public void Step8(string inputs, int result)  
+        => StringCalculator.Add(inputs).Should().Be(result);  
 }
